b2a0ca737e77a4b3f305d9f1bc0c11e966a5635b
Ketan Sakariya <ketan@dotstudiopro.com>
2018-05-15 08:39:54 +0530

    added config file

--- DotstudioSampleApp-iOS/AppDelegate.swift
+++ DotstudioSampleApp-iOS/AppDelegate.swift
@@ -7,7 +7,6 @@
 //
 
 import UIKit
-import DotstudioAPI
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        SPLTRouter.API_KEY = ""
+        DSConfig.setup()
         return true
     }
 
---
+++ source/config/DSConfig.swift
@@ -0,0 +1,21 @@
+//
+//  DSConfig.swift
+//  DotstudioSampleApp
+//
+//  Created by Ketan Sakariya on 14/05/18.
+//  Copyright © 2018 Dotstudioz. All rights reserved.
+//
+
+import Foundation
+import DotstudioAPI
+
+class DSConfig {
+
+    class func setup() {
+        SPLTRouter.API_KEY = ""
+    }
+    
+}
+
+
+
