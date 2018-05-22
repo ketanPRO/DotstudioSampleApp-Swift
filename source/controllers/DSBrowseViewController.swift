4123532c4dadf39e1757936ce87bc39685ad19b8
Ketan Sakariya <ketan@dotstudiopro.com>
2018-05-23 01:11:51 +0530

    fixed bugs

--- source/controllers/DSBrowseViewController.swift
+++ source/controllers/DSBrowseViewController.swift
@@ -81,16 +81,6 @@ open class DSBrowseViewController: SPLTBrowseViewController {
     }
     
     
-    open override func getCollectionViewItemSizeAtIndexPath(_ indexPath: IndexPath) -> CGSize {
-        let size = super.getCollectionViewItemSizeAtIndexPath(indexPath)
-        print(size)
-        return size
-    }
-    open override func getCollectionViewImageSizeAtIndexPath(_ indexPath: IndexPath) -> CGSize {
-        let size = super.getCollectionViewImageSizeAtIndexPath(indexPath)
-        print(size)
-        return size
-    }
 }
 
 //extension DSBrowseViewController: DSHorizontalTableViewCellDelegate {
